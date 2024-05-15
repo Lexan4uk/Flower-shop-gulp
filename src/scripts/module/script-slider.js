f450d0962d77a8b76662e550a872aa7800e7e1d2
Alexey <alkatras46rus@gmail.com>
2024-05-15 16:25:35 +0300

    commit

--- src/scripts/module/script-slider.js
+++ src/scripts/module/script-slider.js
@@ -1,6 +1,27 @@
 window.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.big-swiper', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        var bulletSVGs = [
+          '<circle cx="4" cy="4" r="8" stroke="black" stroke-width="1" fill="red" />',
+          '<circle cx="4" cy="4" r="8" stroke="black" stroke-width="1" fill="red" />',
+          '<circle cx="4" cy="4" r="8" stroke="black" stroke-width="1" fill="red" />',
+          '<circle cx="4" cy="4" r="8" stroke="black" stroke-width="1" fill="red" />',
+        ];
 
-  const swiper = new Swiper('.card-swiper', {
-    slidesPerView: 1
+        return '<span class="' + className + '">' +
+          '<svg width="8" height="8">' +
+          bulletSVGs[index] +
+          '</svg>' +
+          '</span>';
+      },
+    },
   });
+  
+  /*const swiper2 = new Swiper('.card-swiper-e', {
+    init: false,
+    slidePerView: 1
+  });*/
 });
