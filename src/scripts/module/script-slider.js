bbddaf9a42db1bc44310c95c16f3fd1f0b8c8503
Alexey <alkatras46rus@gmail.com>
2024-05-16 16:24:49 +0300

    commit

--- src/scripts/module/script-slider.js
+++ src/scripts/module/script-slider.js
@@ -19,7 +19,25 @@ window.addEventListener('DOMContentLoaded', () => {
       },
     },
   });
-  
+  const swiper2 = new Swiper('.feedback-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    pagination: {
+      el: '.feedback-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      950: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      }
+    }
+  });
+  const swiper3 = new Swiper('.partners-swiper', {
+    width:100,
+    loop: true,
+  });
   /*const swiper2 = new Swiper('.card-swiper-e', {
     init: false,
     slidePerView: 1
